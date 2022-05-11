59c6dab897dbd5c7838618358d104624daf459b0
JeroenHaverkort <jeroen.haverkort@windesheim.nl>
2022-05-11 12:35:43 +0200

    toevoeging en uitbereiding orderfuncties

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args){
         Hoofdscherm h1 = new Hoofdscherm();
-        h1.getStelling().maakOrder();
-        h1.getStelling().voegProductToe(0);
+
         h1.getStelling().voegProductToe(3);
-        h1.getStelling().voegProductToe(3);
-        h1.getStelling().voegProductToe(-1);
-        h1.getStelling().voegProductToe(25);
-        h1.getStelling().voegProductToe(6);
-        h1.getStelling().voegProductToe(9);
-        h1.getStelling().printOrder();
-        h1.getStelling().plaatsOrder();
-        h1.getStelling().printVakken();
+        h1.getStelling().verwijderProduct(3);
+        h1.getStelling().verwijderProduct(3);
+        h1.getStelling().verwijderProduct(25);
     }
 }
--- src/Stelling.java
+++ src/Stelling.java
@@ -28,8 +28,12 @@ public class Stelling {
     public void printOrder(){
         System.out.println("Order: datum " + huidigeOrder.getDatum());
         System.out.println("Producten");
-        for(Vak vak: huidigeOrder.getProducten()){
-            System.out.println("-- " + vak);
+        if (huidigeOrder.getProducten().size()>0) {
+            for (Vak vak : huidigeOrder.getProducten()) {
+                System.out.println("-- " + vak);
+            }
+        } else {
+            System.out.println("--Uw order is nog leeg!");
         }
     }
 
@@ -38,48 +42,76 @@ public class Stelling {
     }
 
     public void plaatsOrder(){
-        //robotacties->
+        if(huidigeOrder != null && huidigeOrder.getProducten().size()>0) {
+            //robotacties->
 
-        //order in de lijst plaatsen
-        orderlijst.add(huidigeOrder);
-        //producten uit order uit de virtuele stelling halen
-        for(Vak vak: huidigeOrder.getProducten()){
-            opslagplekken[vak.getVakId()].setEmpty();
+            //order in de lijst plaatsen
+            orderlijst.add(huidigeOrder);
+            //producten uit order uit de virtuele stelling halen
+            for (Vak vak : huidigeOrder.getProducten()) {
+                opslagplekken[vak.getVakId()].setEmpty();
+            }
+            //order verwijderen uit actieve positie
+            huidigeOrder = null;
+        } else {
+            System.out.println("uw order is nog leeg!");
         }
-        //order verwijderen uit actieve positie
-        huidigeOrder = null;
     }
 
     public void voegProductToe(int productId){
-        //product arraycheck
-        if (productId>=0 && productId<=24) {
-            //er mogen maar drie producten in de order zitten
-            if(huidigeOrder.getProducten().size() < 3) {
-                //ervoor zorgen dat een product niet meerdere keren toegevoegd kan worden aan de order
-                boolean isBeschikbaar = true;
-                for (Vak vak : huidigeOrder.getProducten()) {
-                    if (productId == vak.getVakId()) {
-                        isBeschikbaar = false;
+        //order aanmaken als die nog niet aan is gemaakt
+        if(huidigeOrder==null) {
+            maakOrder();
+        }
+            //product arraycheck
+            if (productId >= 0 && productId <= 24) {
+                //er mogen maar drie producten in de order zitten
+                if (huidigeOrder.getProducten().size() < 3) {
+                    //ervoor zorgen dat een product niet meerdere keren toegevoegd kan worden aan de order
+                    boolean isBeschikbaar = true;
+                    for (Vak vak : huidigeOrder.getProducten()) {
+                        if (productId == vak.getVakId()) {
+                            isBeschikbaar = false;
+                        }
                     }
-                }
 
-                if (isBeschikbaar) {
-                    //product wordt toegevoegd
-                    huidigeOrder.getProducten().add(opslagplekken[productId]);
-                    System.out.println("product " + productId + " is aan je order toegevoegd");
-                    //TSP algoritme --> producten sorteren om het pad te bepalen
+                    if (isBeschikbaar) {
+                        //product wordt toegevoegd
+                        huidigeOrder.getProducten().add(opslagplekken[productId]);
+                        System.out.println("product " + productId + " is aan je order toegevoegd");
+                        //TSP algoritme --> producten sorteren om het pad te bepalen
 
+                    } else {
+                        //product niet toevoegen --> zit al in order
+                        System.out.println("product " + productId + " zit al in je order!");
+                    }
                 } else {
-                    //product niet toevoegen --> zit al in order
-                    System.out.println("product " + productId + " zit al in je order!");
+                    System.out.println("je order zit vol!");
                 }
             } else {
-                System.out.println("je order zit vol!");
+                //productId valt buiten array
+                System.out.println("productId moet binnen de stelling zijn([0,24])");
+            }
+            printOrder();
+    }
+
+    public void verwijderProduct(int productId){
+        if(productId>=0 && productId <=24) {
+            boolean isVerwijderd = false;
+            for (int i = 0; i < huidigeOrder.getProducten().size(); i++) {
+                if (productId == huidigeOrder.getProducten().get(i).getVakId()) {
+                    huidigeOrder.getProducten().remove(i);
+                    isVerwijderd = true;
+                    System.out.println("product " + productId + " is verwijderd uit uw order");
+                }
+            }
+            if (!isVerwijderd) {
+                System.out.println("product" + productId + " zit niet in uw order en kan dus niet verwijderd worden");
             }
         } else {
-            //productId valt buiten array
-            System.out.println("productId moet binnen de stelling zijn([0,24])");
+            System.out.println("product " + productId + " staat niet in de stelling");
         }
+        printOrder();
     }
 
 }
