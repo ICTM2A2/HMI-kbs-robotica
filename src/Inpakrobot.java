00726d0ef22420ad6cecb0f945e9e5c2c9f7dd9f
luximus-hunter <thomaslipman10@gmail.com>
2022-06-02 11:07:18 +0200

    enable robot

--- src/Inpakrobot.java
+++ src/Inpakrobot.java
@@ -4,11 +4,11 @@ public class Inpakrobot {
     Arduino inpakRobot;
 
     public Inpakrobot() {
-//        inpakRobot = new Arduino("COM5", 9600);
-//        inpakRobot.openConnection();
+        inpakRobot = new Arduino("COM5", 9600);
+        inpakRobot.openConnection();
     }
 
     public void verstuurRichting(String richting) {
-//        inpakRobot.serialWrite(richting);
+        inpakRobot.serialWrite(richting);
     }
 }
--- src/Pakrobot.java
+++ src/Pakrobot.java
@@ -4,22 +4,15 @@ public class Pakrobot {
     Arduino pakRobot;
 
     public Pakrobot() {
-//        pakRobot = new Arduino("COM3", 9600);
-//        pakRobot.openConnection();
+        pakRobot = new Arduino("COM3", 9600);
+        pakRobot.openConnection();
     }
 
     public void verstuurCoord(String coord, int count) {
-//        pakRobot.serialWrite(coord);
+        pakRobot.serialWrite(coord);
 
-        for (int i = 0; i < count; i++) {
-//            while (!pakRobot.serialRead().trim().contains("Moved")) {
-//            }
-
-            System.out.println("Moved");
+        while (!pakRobot.serialRead().trim().contains("Done")) {
         }
-
-//        while (!pakRobot.serialRead().trim().contains("Done!")) {
-//        }
 
         System.out.println("Done!");
     }
